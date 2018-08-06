ebc1e5d7b659ada6dcc2ceeca13bd4dedaae3554
artem <temoff7@gmail.com>
2018-08-06 22:51:02 +0300

    Initial Commit

---
+++ Currency/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Currency
+//
+//  Created by Artem on 06/08/2018.
+//  Copyright © 2018 Artem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
